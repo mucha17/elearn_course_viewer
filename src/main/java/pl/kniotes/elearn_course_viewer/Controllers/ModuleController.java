1efbb99d05a7ddfa9aaf6b1516b60cc9f2941e25
mucha17 <46112158+mucha17@users.noreply.github.com>
2021-01-31 01:02:31 +0100

    Bugproof ModuleController

--- src/main/java/pl/kniotes/elearn_course_viewer/Controllers/ModuleController.java
+++ src/main/java/pl/kniotes/elearn_course_viewer/Controllers/ModuleController.java
@@ -2,7 +2,9 @@ package pl.kniotes.elearn_course_viewer.Controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import pl.kniotes.elearn_course_viewer.Models.Module.Content;
 import pl.kniotes.elearn_course_viewer.Models.Module.ContentType;
 import pl.kniotes.elearn_course_viewer.Models.Module.Lesson;
@@ -29,7 +31,11 @@ public class ModuleController {
     @PostMapping("/api/modules")
     public Module postModule(String name, String description) {
         Module module = new Module();
-        module.setName(name);
+        if (name != null && !name.isBlank()) {
+            module.setName(name);
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Name is not set");
+        }
         module.setDescription(description);
         return this.moduleRepository.save(module);
     }
@@ -44,8 +50,15 @@ public class ModuleController {
     @PutMapping("/api/modules/{id}")
     public Module updateModule(@PathVariable("id") String id, String name, String description) {
         Module module = this.moduleRepository.findModuleById(id);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
         if (name != null && !name.isBlank()) {
             module.setName(name);
+        } else {
+            if (module.getName() == null || module.getName().isBlank()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module name seems to be empty. Set the name in order to update");
+            }
         }
         if (description != null && !description.isBlank()) {
             module.setDescription(description);
@@ -63,87 +76,172 @@ public class ModuleController {
     @CrossOrigin("*")
     @GetMapping("/api/modules/{moduleId}/lessons")
     public HashMap<Integer, Lesson> getLessons(@PathVariable("moduleId") String moduleId) {
-        return this.moduleRepository.findModuleById(moduleId).getLessons();
+        Module module = this.moduleRepository.findModuleById(moduleId);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        return module.getLessons();
     }
 
     @CrossOrigin("*")
     @PostMapping("/api/modules/{moduleId}/lessons")
     public Lesson postLesson(@PathVariable("moduleId") String moduleId, String name, String description) {
         Module module = this.moduleRepository.findModuleById(moduleId);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        Lesson lesson = new Lesson();
+        if (name != null && !name.isBlank()) {
+            lesson.setName(name);
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Name is not set");
+        }
+        if (description != null && !description.isBlank()) {
+            lesson.setDescription(description);
+        }
         Integer key = module.getLessons().size();
-        module.getLessons().put(key, new Lesson(name, description));
+        module.getLessons().put(key, lesson);
         this.moduleRepository.save(module);
         return this.moduleRepository.findModuleById(moduleId).getLessons().get(key);
     }
 
     @CrossOrigin("*")
-    @GetMapping("/api/modules/{moduleId}/lessons/{lessonId}")
-    public Lesson getLesson(@PathVariable("moduleId") String moduleId, @PathVariable("lessonId") Integer lessonId) {
-        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonId);
+    @GetMapping("/api/modules/{moduleId}/lessons/{lessonKey}")
+    public Lesson getLesson(@PathVariable("moduleId") String moduleId, @PathVariable("lessonKey") Integer lessonKey) {
+        Module module = this.moduleRepository.findModuleById(moduleId);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        return module.getLessons().get(lessonKey);
     }
 
     @CrossOrigin("*")
-    @PutMapping("/api/modules/{moduleId}/lessons/{lessonId}")
-    public Lesson updateLesson(@PathVariable("moduleId") String moduleId, @PathVariable("lessonId") Integer lessonId, String name, String description) {
+    @PutMapping("/api/modules/{moduleId}/lessons/{lessonKey}")
+    public Lesson updateLesson(@PathVariable("moduleId") String moduleId, @PathVariable("lessonKey") Integer lessonKey, String name, String description) {
         Module module = this.moduleRepository.findModuleById(moduleId);
-        Lesson lesson = module.getLessons().get(lessonId);
-        lesson.setName(name);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        Lesson lesson = module.getLessons().get(lessonKey);
+        if (lesson == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lesson is invalid");
+        }
+        if (name != null && !name.isBlank()) {
+            lesson.setName(name);
+        } else {
+            if (lesson.getName() == null || lesson.getName().isBlank()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course name seems to be empty. Set the name in order to update");
+            }
+        }
         lesson.setDescription(description);
         this.moduleRepository.save(module);
-        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonId);
+        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonKey);
     }
 
     @CrossOrigin("*")
-    @DeleteMapping("/api/modules/{moduleId}/lessons/{lessonId}")
-    public Boolean deleteLesson(@PathVariable("moduleId") String moduleId, @PathVariable("lessonId") Integer lessonId) {
+    @DeleteMapping("/api/modules/{moduleId}/lessons/{lessonKey}")
+    public Boolean deleteLesson(@PathVariable("moduleId") String moduleId, @PathVariable("lessonKey") Integer lessonKey) {
         Module module = this.moduleRepository.findModuleById(moduleId);
-        module.getLessons().remove(lessonId);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        if (module.getLessons().get(lessonKey) == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lesson is invalid");
+        }
+        module.getLessons().remove(lessonKey);
         this.moduleRepository.save(module);
-        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonId) == null;
+        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonKey) == null;
     }
 
     @CrossOrigin("*")
-    @GetMapping("/api/modules/{moduleId}/lessons/{lessonId}/contents")
-    public HashMap<Integer, Content> getContents(@PathVariable("moduleId") String moduleId, @PathVariable("lessonId") Integer lessonId) {
-        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonId).getContents();
-    }
-
-    @CrossOrigin("*")
-    @PostMapping("/api/modules/{moduleId}/lessons/{lessonId}/contents")
-    public Content postContent(@PathVariable("moduleId") String moduleId, @PathVariable("lessonId") Integer lessonId, ContentType type, String url) {
+    @GetMapping("/api/modules/{moduleId}/lessons/{lessonKey}/contents")
+    public HashMap<Integer, Content> getContents(@PathVariable("moduleId") String moduleId, @PathVariable("lessonKey") Integer lessonKey) {
         Module module = this.moduleRepository.findModuleById(moduleId);
-        Lesson lesson = module.getLessons().get(lessonId);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        Lesson lesson = module.getLessons().get(lessonKey);
+        if (lesson == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lesson is invalid");
+        }
+        return lesson.getContents();
+    }
+
+    @CrossOrigin("*")
+    @PostMapping("/api/modules/{moduleId}/lessons/{lessonKey}/contents")
+    public Content postContent(@PathVariable("moduleId") String moduleId, @PathVariable("lessonKey") Integer lessonKey, ContentType type, String url) {
+        if (type == null || !(type.equals(ContentType.ARTICLE) || type.equals(ContentType.PODCAST) || type.equals(ContentType.VIDEO))) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Type is not set or is not applicable");
+        }
+        Module module = this.moduleRepository.findModuleById(moduleId);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        if (module.getLessons().get(lessonKey) == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lesson is invalid");
+        }
+        Lesson lesson = module.getLessons().get(lessonKey);
+        if (lesson == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lesson is not set.");
+        }
         Integer key = lesson.getContents().size();
         lesson.getContents().put(key, new Content(type, url));
         this.moduleRepository.save(module);
-        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonId).getContents().get(key);
+        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonKey).getContents().get(key);
     }
 
     @CrossOrigin("*")
-    @GetMapping("/api/modules/{moduleId}/lessons/{lessonId}/contents/{contentId}")
-    public Content getContent(@PathVariable("moduleId") String moduleId, @PathVariable("lessonId") Integer lessonId, @PathVariable("contentId") Integer contentId) {
-        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonId).getContents().get(contentId);
-    }
-
-    @CrossOrigin("*")
-    @PutMapping("/api/modules/{moduleId}/lessons/{lessonId}/contents/{contentId}")
-    public Content updateContent(@PathVariable("moduleId") String moduleId, @PathVariable("lessonId") Integer lessonId, @PathVariable("contentId") Integer contentId, ContentType type, String url) {
+    @GetMapping("/api/modules/{moduleId}/lessons/{lessonKey}/contents/{contentId}")
+    public Content getContent(@PathVariable("moduleId") String moduleId, @PathVariable("lessonKey") Integer lessonKey, @PathVariable("contentId") Integer contentId) {
         Module module = this.moduleRepository.findModuleById(moduleId);
-        Lesson lesson = module.getLessons().get(lessonId);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        Lesson lesson = module.getLessons().get(lessonKey);
+        if (lesson == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lesson is not set.");
+        }
+        return lesson.getContents().get(contentId);
+    }
+
+    @CrossOrigin("*")
+    @PutMapping("/api/modules/{moduleId}/lessons/{lessonKey}/contents/{contentId}")
+    public Content updateContent(@PathVariable("moduleId") String moduleId, @PathVariable("lessonKey") Integer lessonKey, @PathVariable("contentId") Integer contentId, ContentType type, String url) {
+        Module module = this.moduleRepository.findModuleById(moduleId);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        if (module.getLessons().get(lessonKey) == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lesson is invalid");
+        }
+        Lesson lesson = module.getLessons().get(lessonKey);
         Content content = lesson.getContents().get(contentId);
-        content.setType(type);
+        if (type != null && (type.equals(ContentType.ARTICLE) || type.equals(ContentType.PODCAST) || type.equals(ContentType.VIDEO))) {
+            content.setType(type);
+        } else {
+            if (content.getType() == null || !(content.getType().equals(ContentType.ARTICLE) || content.getType().equals(ContentType.PODCAST) || content.getType().equals(ContentType.VIDEO))) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lesson content type seems to be not set or is not applicable. Set the content type in order to update.");
+            }
+        }
+
         content.setUrl(url);
         this.moduleRepository.save(module);
-        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonId).getContents().get(contentId);
+        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonKey).getContents().get(contentId);
     }
 
     @CrossOrigin("*")
-    @DeleteMapping("/api/modules/{moduleId}/lessons/{lessonId}/{contentId}")
-    public Boolean deleteLesson(@PathVariable("moduleId") String moduleId, @PathVariable("lessonId") Integer lessonId, @PathVariable("contentId") Integer contentId) {
+    @DeleteMapping("/api/modules/{moduleId}/lessons/{lessonKey}/{contentId}")
+    public Boolean deleteLesson(@PathVariable("moduleId") String moduleId, @PathVariable("lessonKey") Integer lessonKey, @PathVariable("contentId") Integer contentId) {
         Module module = this.moduleRepository.findModuleById(moduleId);
-        module.getLessons().get(lessonId).getContents().remove(contentId);
+        if (module == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module is invalid");
+        }
+        if (module.getLessons().get(lessonKey) == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lesson is invalid");
+        }
+        module.getLessons().get(lessonKey).getContents().remove(contentId);
         this.moduleRepository.save(module);
-        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonId).getContents().get(contentId) == null;
+        return this.moduleRepository.findModuleById(moduleId).getLessons().get(lessonKey).getContents().get(contentId) == null;
     }
 
 }
